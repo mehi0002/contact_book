9a368da2279701c196237513abfa6d084b95ce35
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2024-11-20 18:32:20 -0500

    Installed dependancies and linked firebase

---
+++ src/db.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIbQHmYZiz9L2pM1VwFbbI4PPj93fLZEc",
+  authDomain: "mtm6404-mehi0002.firebaseapp.com",
+  projectId: "mtm6404-mehi0002",
+  storageBucket: "mtm6404-mehi0002.firebasestorage.app",
+  messagingSenderId: "457446920004",
+  appId: "1:457446920004:web:45ac41a2603aaef37c5111"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
