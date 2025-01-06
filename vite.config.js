23068289eb72d917c6152f19675bb48cf58839ad
mehi0002 <90566268+mehi0002@users.noreply.github.com>
2025-01-06 13:05:32 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://github.com/mehi0002/contact_book"
+  plugins: [react()]
 })
